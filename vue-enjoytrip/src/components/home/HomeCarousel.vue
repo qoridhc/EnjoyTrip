48da780fb7f34159ad435df18f2ca890d8d468d2
허동원 <heo_dongwon@naver.com>
2024-05-10 17:17:51 +0900

    modify main UI, add assests

---
+++ vue-enjoytrip/src/components/home/HomeCarousel.vue
@@ -0,0 +1,34 @@
+<script setup>
+
+</script>
+
+<template>
+    <div id="carouselExampleControls" class="carousel slide" data-ride="carousel w-100 h-100">
+        <div class="carousel-inner d-block">
+            <div class="carousel-item active">
+                <img src="@/assets/img-main/carousel1.jpg" alt="First slide" />
+            </div>
+            <div class="carousel-item">
+                <img src="@/assets/img-main/carousel2.jpg" alt="First slide" />
+            </div>
+            <div class="carousel-item">
+                <img src="@/assets/img-main/carousel3.jpg" alt="First slide" />
+            </div>
+            <div class="carousel-item">
+                <img src="@/assets/img-main/carousel4.jpg" alt="First slide" />
+            </div>
+        </div>
+        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls"
+            data-bs-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <!-- <span class="visually-hidden"></span> -->
+        </button>
+        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls"
+            data-bs-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <!-- <span class="visually-hidden"></span> -->
+        </button>
+    </div>
+</template>
+
+<style scoped></style>
---
+++ vue-enjoytrip/src/components/home/HomeInfo1.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="container min-vw-100 py-3 gray">
+        <div class="row gx-5 pb-5 justify-content-center">
+            <div class="col-lg-12 col-xl-10">
+                <div class="text-center">
+                    <h2 class="fw-bolder">지역별 관광지 정보</h2>
+                    <h4 class="mt-5">원하는 테마별로 얻는 관광지 정보</h4>
+                </div>
+                <div class="d-flex justify-content-center">
+                    <img class="mt-5" style="width: 45%" src="@/assets/img-main/info1.png" alt="" />
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
