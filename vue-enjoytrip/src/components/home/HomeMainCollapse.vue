79edae22a32d83b1c81e17878bf8c13bd5f4c029
허동원 <heo_dongwon@naver.com>
2024-05-14 09:01:09 +0900

    HomeCollapse, Modal update

--- vue-enjoytrip/src/components/home/HomeMainCollapse.vue
+++ vue-enjoytrip/src/components/home/HomeMainCollapse.vue
@@ -1,10 +1,14 @@
 <template>
-<div class="">
     <div class="collapse w-100" id="startInputCollapse">
         <div class="input-group">
-            <input class="form-control me-3" type="text" placeholder="어디로 여행을 떠나시나요?">
-            <button type="button" class="btn btn-dark" data-bs-toggle="modal" data-bs-target="#centerModal">입력</button>
+            <input class="form-control border border-dark rounded-pill me-3" type="text" placeholder="어디로 여행을 떠나시나요?">
+            <button type="button" class="btn rounded-pill" data-bs-toggle="modal" data-bs-target="#centerModal">입력</button>
         </div>
     </div>
-</div>
 </template>
+
+<style scoped>
+.btn {
+    border: 1px solid black;
+}
+</style>
--- vue-enjoytrip/src/components/home/HomeMainModal.vue
+++ vue-enjoytrip/src/components/home/HomeMainModal.vue
@@ -1,10 +1,11 @@
 <template>
+
     <!-- 여행지 검색 MODAL -->
     <div class="modal fade" id="centerModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
         <div class="modal-dialog modal-dialog-centered">
             <div class="modal-content">
                 <div class="modal-header">
-                    <h5 class="modal-title">Modal title</h5>
+                    <h4 class="modal-title noto-sans">대한민국 제주</h4>
                     <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                 </div>
                 <div class="modal-body">
@@ -18,3 +19,9 @@
         </div>
     </div>
 </template>
+
+<style scoped>
+.noto-sans {
+    font-family: 'NotoSansKR-Regular';
+}
+</style>
