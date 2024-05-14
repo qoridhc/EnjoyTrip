0b073371f0f9bb14ca9c1f61c69e0be0dc97b750
허동원 <heo_dongwon@naver.com>
2024-05-14 16:11:57 +0900

    modal update

--- vue-enjoytrip/src/main.js
+++ vue-enjoytrip/src/main.js
@@ -6,9 +6,15 @@ import router from "./router";
 
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap";
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faPlane, faPassport, faBolt, faClock } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 
 const app = createApp(App);
 
+library.add(faPlane, faPassport, faBolt, faClock);
+
+app.component('font-awesome-icon', FontAwesomeIcon);
 // app.use(createPinia());
 app.use(router);
 
