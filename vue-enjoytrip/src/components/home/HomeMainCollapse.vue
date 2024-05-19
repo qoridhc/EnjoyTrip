a53b46ae083e3ee111e655362924f8d7af9304d3
허동원 <heo_dongwon@naver.com>
2024-05-19 15:24:35 +0900

    feat: 자동완성 기능 추가

--- vue-enjoytrip/src/components/home/HomeMainCollapse.vue
+++ vue-enjoytrip/src/components/home/HomeMainCollapse.vue
@@ -1,4 +1,6 @@
 <script setup>
+import { ref, computed } from 'vue';
+
 const attractionList = [
     { "sido_code": "1", "sido_name": "서울" }, 
     { "sido_code": "2", "sido_name": "인천" }, 
@@ -17,27 +19,64 @@ const attractionList = [
     { "sido_code": "37", "sido_name": "전라북도" }, 
     { "sido_code": "38", "sido_name": "전라남도" }, 
     { "sido_code": "39", "sido_name": "제주도" }
-]
+];
 
+const inputRoute = ref("");
+
+const filteredAttractionList = computed(() => {
+    const searchTerm = inputRoute.value.toLowerCase();
+    return attractionList.filter(attraction => attraction.sido_name.toLowerCase().includes(searchTerm));
+});
+
+const handleEnter = () => {
+    document.querySelector('#inputButton').click();
+};
 </script>
 
 <template>
     <div class="collapse w-100" id="startInputCollapse">
         <div class="input-group">
-            <input class="form-control border border-dark rounded-pill me-3" type="text" placeholder="어디로 여행을 떠나시나요?"
-                list="attraction-list">
-            <button type="button" class="btn rounded-pill" data-bs-toggle="modal"
-                data-bs-target="#centerModal">입력</button>
-
-            <datalist id="attraction-list" >
-                <option :value="sido.sido_name" v-for="(sido, index) in attractionList" :key="index">{{ sido.sido_name }}</option>
-            </datalist>
+            <input 
+                type="text" 
+                class="form-control border border-dark rounded-pill me-3 custom-input" 
+                placeholder="어디로 여행을 떠나시나요?"
+                v-model="inputRoute">
         </div>
+        <ul v-if="inputRoute" class="list-group position-absolute">
+            <li 
+                class="list-group-item" 
+                v-for="(sido, index) in filteredAttractionList" :key="index"
+                data-bs-toggle="modal"
+                data-bs-target="#centerModal"
+            >
+                {{ sido.sido_name }}
+            </li>
+        </ul>
     </div>
 </template>
 
 <style scoped>
+.custom-input {
+    padding-left: 20px;
+    height: 50px;
+    font-size: 1.2em; /* 텍스트 크기 조정 */
+}
 .btn {
     border: 1px solid black;
+}
+.list-group {
+    max-height: 200px;
+    overflow-y: auto;
+    width: 26.6%;
+    background-color: #f8f9fa;
+    border: 1px solid #ddd;
+    border-radius: 0.25rem;
+    margin-top: 5px;
+    padding: 0;
+}
+.list-group-item {
+    border: none;
+    padding: 10px 15px;
+    background-color: #f8f9fa;
 }
 </style>
---
+++ vue-enjoytrip/src/stores/route.js
@@ -0,0 +1,10 @@
+import { ref, computed } from 'vue'
+import { defineStore } from 'pinia'
+
+export const useRouteStore = defineStore('route', () => {
+
+    const route = ref("")
+
+
+    return {  }
+})
