30716330dbd032fba5a9d097a982f836c7c9660e
이성현 <vkse1934@naver.com>
2024-05-21 11:23:44 +0900

    fix : 메인 이미지 수정

--- vue-enjoytrip/src/components/home/HomeInfo1.vue
+++ vue-enjoytrip/src/components/home/HomeInfo1.vue
@@ -1,22 +1,19 @@
-
 <!-- 지역별 관광지 정보 -->
 <template>
-    <div class="container min-vw-100 py-3 gray">
-        <div class="row gx-5 py-4 justify-content-center">
-            <div class="col-lg-12 col-xl-10">
-
-                <!-- 텍스트 -->
-                <div class="text-center">
-                    <h2 class="fw-bolder">지역별 관광지 정보</h2>
-                    <h4 class="mt-4">원하는 테마별로 얻는 관광지 정보</h4>
-                </div>
-
-                <!-- 이미지 -->
-                <div class="d-flex justify-content-center">
-                    <img class="mt-4" style="width: 40%" src="@/assets/img-main/info1.png" alt="" />
-                </div>
-            </div>
+  <div class="container min-vw-100 py-3 gray">
+    <div class="row gx-5 py-4 justify-content-center">
+      <div class="col-lg-12 col-xl-10">
+        <!-- 텍스트 -->
+        <div class="text-center">
+          <h2 class="fw-bolder">지역별 관광지 정보</h2>
+          <h4 class="mt-4">원하는 테마별로 얻는 관광지 정보</h4>
         </div>
-    </div>
-</template>
 
+        <!-- 이미지 -->
+        <div class="d-flex justify-content-center">
+          <img class="mt-4" style="width: 40%" src="@/assets/img-main/main1.png" alt="" />
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
--- vue-enjoytrip/src/components/home/HomeInfo2.vue
+++ vue-enjoytrip/src/components/home/HomeInfo2.vue
@@ -1,27 +1,25 @@
 <template>
-    <div class="container min-vw-100 gray pd">
-        <div class="row justify-content-center pt-5 pb-5">
-
-            <!-- 왼쪽 텍스트 -->
-            <div class="col-3" style="margin: auto 0px;">
-                <div class="py-5 text-start">
-                    <h2 class="fw-bolder display-5">나의 여행 계획</h2>
-                    <h4 class="mt-5 dispaly-6 text-black-75">여행 루트를 만들고 최적의 경로로 이동해요</h4>
-                    <h4 class="mt-2 dispaly-6 text-black-75">플랜을 다른 사람과 공유해요</h4>
-                </div>
-            </div>
-
-            <!-- 오른쪽 그림 -->
-            <div class="col-4">
-                <img style="width: 130%;" src="@/assets/img-main/mytrip.png" alt="" />
-            </div>
+  <div class="container min-vw-100 gray pd">
+    <div class="row justify-content-center pt-5 pb-5">
+      <!-- 왼쪽 텍스트 -->
+      <div class="col-3" style="margin: auto 0px">
+        <div class="py-5 text-start">
+          <h2 class="fw-bolder display-5">나의 여행 계획</h2>
+          <h4 class="mt-5 dispaly-6 text-black-75">여행 루트를 만들고 최적의 경로로 이동해요</h4>
+          <h4 class="mt-2 dispaly-6 text-black-75">플랜을 다른 사람과 공유해요</h4>
         </div>
-    </div>
-</template>
+      </div>
 
+      <!-- 오른쪽 그림 -->
+      <div class="col-4">
+        <img style="width: 130%" src="@/assets/img-main/main2.png" alt="" />
+      </div>
+    </div>
+  </div>
+</template>
 
 <style scoped>
 .pd {
-    padding: 100px 0px;
+  padding: 100px 0px;
 }
 </style>
